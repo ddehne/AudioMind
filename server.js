e991c14382596542fe8c9013146365d431e0f36a
david dehne <dehne.david@gmail.com>
2020-05-23 13:23:22 -0500

    removing api endpoints

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const app = express();
 app.use(express.static("./client/build"));
 
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
 
-app.use(morgan('tiny'));
-
+// app.use(morgan('tiny'));
+console.log("HELLO!!!")
 
 
 //mongoURI
@@ -39,156 +39,156 @@ conn.once('open', () => {
 
 //create storage object/engine (action='/upload' on html form inits this)
 
-const storage = new GridFSStorage({
-    url: mongoURI,
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) {
-                    return reject(err);
-                }
-                // const filename = buf.toString('hex') + path.extname(file.originalname);
-                const fileInfo = {
-                    filename: file.originalname,
-                    bucketName: 'uploads'
-                };
-                resolve(fileInfo);
-            })
-        });
-    }
-});
-const upload = multer({ storage });
+// const storage = new GridFSStorage({
+//     url: mongoURI,
+//     file: (req, file) => {
+//         return new Promise((resolve, reject) => {
+//             crypto.randomBytes(16, (err, buf) => {
+//                 if (err) {
+//                     return reject(err);
+//                 }
+//                 // const filename = buf.toString('hex') + path.extname(file.originalname);
+//                 const fileInfo = {
+//                     filename: file.originalname,
+//                     bucketName: 'uploads'
+//                 };
+//                 resolve(fileInfo);
+//             })
+//         });
+//     }
+// });
+// const upload = multer({ storage });
 
-// @route GET /
-// desxcription lasds form
+// // @route GET /
+// // desxcription lasds form
 
-app.get('/api/', (req,res) => {
-    console.log("cabbage")
-    gfs.files.find().toArray((err, files) => {
-        if(!files || files.length === 0) {
-            res.json([])
-        } else {
-            files.map(file => {
-                if(file.contentType === 'video/x-matroska') 
-                {
-                    file.isVideo = true;
-                } else {
-                    file.isVideo = false;
-                } if(file.contentType === 'audio/wav' || file.contentType === 'audio/mpeg' 
-                || file.contentType === 'audio/mp3' || file.contentType === 'audio/mpeg-4'
-                || file.contentType === 'audio/m4a' || file.contentType === 'audio/mp4' 
-                || file.contentType === 'audio/x-m4a')
-                {
-                    file.isAudio = true;
-                } else {
-                    file.isAudio = false;
-                }
-            });
-            res.send(files)
-        }        
-    });
-});
+// app.get('/api/', (req,res) => {
+//     console.log("cabbage")
+//     gfs.files.find().toArray((err, files) => {
+//         if(!files || files.length === 0) {
+//             res.json([])
+//         } else {
+//             files.map(file => {
+//                 if(file.contentType === 'video/x-matroska') 
+//                 {
+//                     file.isVideo = true;
+//                 } else {
+//                     file.isVideo = false;
+//                 } if(file.contentType === 'audio/wav' || file.contentType === 'audio/mpeg' 
+//                 || file.contentType === 'audio/mp3' || file.contentType === 'audio/mpeg-4'
+//                 || file.contentType === 'audio/m4a' || file.contentType === 'audio/mp4' 
+//                 || file.contentType === 'audio/x-m4a')
+//                 {
+//                     file.isAudio = true;
+//                 } else {
+//                     file.isAudio = false;
+//                 }
+//             });
+//             res.send(files)
+//         }        
+//     });
+// });
 
-// @routse /upload
-// description uploads file (name=file on html form connects to 'file' here)
+// // @routse /upload
+// // description uploads file (name=file on html form connects to 'file' here)
 
-app.post('/upload', upload.single('file'), (req, res) => {
-    // res.json({file: req.file})
-    res.redirect('/');
-})
+// app.post('/upload', upload.single('file'), (req, res) => {
+//     // res.json({file: req.file})
+//     res.redirect('/');
+// })
 
-// @route GET /files
-// description displays all files in JSON
-app.get('/files', (req, res) => {
-    gfs.files.find().toArray((err, files) => {
-        // Check if files
-        if(!files || files.length === 0) {
-            return res.status(404).json({
-                err: 'no files exist'
-            });
-        }
+// // @route GET /files
+// // description displays all files in JSON
+// app.get('/files', (req, res) => {
+//     gfs.files.find().toArray((err, files) => {
+//         // Check if files
+//         if(!files || files.length === 0) {
+//             return res.status(404).json({
+//                 err: 'no files exist'
+//             });
+//         }
 
-        //files exist
-        return res.json(files);
-    });
-})
+//         //files exist
+//         return res.json(files);
+//     });
+// })
 
-// @route GET /files/:filename
-// description displays single files in JSON
-app.get('/files/:filename', (req, res) => {
-    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
-        //check if files
-        if (!file || file.length === 0) {
-            return res.status(404).json({
-                err: 'no files exist'
-            });
-        }
+// // @route GET /files/:filename
+// // description displays single files in JSON
+// app.get('/files/:filename', (req, res) => {
+//     gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+//         //check if files
+//         if (!file || file.length === 0) {
+//             return res.status(404).json({
+//                 err: 'no files exist'
+//             });
+//         }
 
-        //file exists
-        return res.json(file);
-    });
-    });
+//         //file exists
+//         return res.json(file);
+//     });
+//     });
 
-// @route GET /files/:filename
-// description displays a video in JSON
-app.get('/video/:filename', (req, res) => {
-    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
-        //check if files
-        if (!file || file.length === 0) {
-            return res.status(404).json({
-                err: 'no files exist'
-            });
-        }
-        //check if video
-        if (file.contentType === 'video/x-matroska') {
-            //REad output to brower
-            const readstream = gfs.createReadStream(file.filename);
-            readstream.pipe(res);
-        } else {
-            res.status(404).json({
-                err: 'Not a Video'
-            });
-        }
-    });
-    });
+// // @route GET /files/:filename
+// // description displays a video in JSON
+// app.get('/video/:filename', (req, res) => {
+//     gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+//         //check if files
+//         if (!file || file.length === 0) {
+//             return res.status(404).json({
+//                 err: 'no files exist'
+//             });
+//         }
+//         //check if video
+//         if (file.contentType === 'video/x-matroska') {
+//             //REad output to brower
+//             const readstream = gfs.createReadStream(file.filename);
+//             readstream.pipe(res);
+//         } else {
+//             res.status(404).json({
+//                 err: 'Not a Video'
+//             });
+//         }
+//     });
+//     });
 
-// @route GET /files/:filename
-// description displays all audio files in JSON
-app.get('/audio/:filename', (req, res) => {
-    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
-        //check if files
-        if (!file || file.length === 0) {
-            return res.status(404).json({
-                err: 'no files exist'
-            });
-        }
-        //check if video
-        if (file.contentType === 'audio/wav' || file.contentType === 'audio/mpeg' 
-        || file.contentType === 'audio/mp3' || file.contentType === 'audio/mpeg-4'
-        || file.contentType === 'audio/m4a' || file.contentType === 'audio/mp4' 
-        || file.contentType === 'audio/x-m4a') {
-            //REad output to brower
-            const readstream = gfs.createReadStream(file.filename);
-            readstream.pipe(res);
-        } else {
-            res.status(404).json({
-                err: 'Not audio'
-            });
-        }
-    });
-    });
+// // @route GET /files/:filename
+// // description displays all audio files in JSON
+// app.get('/audio/:filename', (req, res) => {
+//     gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+//         //check if files
+//         if (!file || file.length === 0) {
+//             return res.status(404).json({
+//                 err: 'no files exist'
+//             });
+//         }
+//         //check if video
+//         if (file.contentType === 'audio/wav' || file.contentType === 'audio/mpeg' 
+//         || file.contentType === 'audio/mp3' || file.contentType === 'audio/mpeg-4'
+//         || file.contentType === 'audio/m4a' || file.contentType === 'audio/mp4' 
+//         || file.contentType === 'audio/x-m4a') {
+//             //REad output to brower
+//             const readstream = gfs.createReadStream(file.filename);
+//             readstream.pipe(res);
+//         } else {
+//             res.status(404).json({
+//                 err: 'Not audio'
+//             });
+//         }
+//     });
+//     });
 
-// @route DELETE /files/:id
-//description delets file
-app.delete('/files/:id', (req,res) =>{
-    gfs.remove({_id: req.params.id, root: 'uploads'}, (err, gridStore) => {
-        if(err) {
-            return res.status(404).json({ err: err });
-        }
+// // @route DELETE /files/:id
+// //description delets file
+// app.delete('/files/:id', (req,res) =>{
+//     gfs.remove({_id: req.params.id, root: 'uploads'}, (err, gridStore) => {
+//         if(err) {
+//             return res.status(404).json({ err: err });
+//         }
 
-        res.redirect('/')
-    })
-})
+//         res.redirect('/')
+//     })
+// })
 
 // if (process.env.NODE_ENV === "production") {
     // app.use(express.static("client/build"));
